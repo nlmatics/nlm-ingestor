c2218a1d738e2669c35405573a24c7dd81376d52
Ambika Sukla <ambika.sukla@nlmatics.com>
2024-01-23 20:48:24 -0500

    run the server without debug

--- nlm_ingestor/ingestion_daemon/__main__.py
+++ nlm_ingestor/ingestion_daemon/__main__.py
@@ -68,7 +68,7 @@ def parse_document(
 
 def main():
     logger.info("Starting ingestor service..")
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=False)
 
 if __name__ == "__main__":
     main()
