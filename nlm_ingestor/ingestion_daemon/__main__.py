418029475f5c7799e14f86db999c36b2cddd8394
moveyor <102074555+moveyor@users.noreply.github.com>
2024-04-09 19:31:58 +0100

    Update __main__.py
    Enable GET method on health check

--- nlm_ingestor/ingestion_daemon/__main__.py
+++ nlm_ingestor/ingestion_daemon/__main__.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 logger = logging.getLogger(__name__)
 logger.setLevel(cfg.log_level())
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def health_check():
     return 'Service is running', 200
 
